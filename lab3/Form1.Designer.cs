2ad2db421685d2a29c5e12fff446ccbbd60896c1
SValeks <SValeksandra6832@yandex.ru>
2023-12-13 23:08:17 +0400

    first commit

---
+++ lab3/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿
+namespace lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cipherAnswer = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cipherKey = new System.Windows.Forms.TextBox();
+            this.cipherMessage = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.decipherAnswer = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.decipherMessage = new System.Windows.Forms.TextBox();
+            this.decipherKey = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.закодироватьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.раскодироватьToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cipherAnswer);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cipherKey);
+            this.groupBox1.Controls.Add(this.cipherMessage);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(368, 411);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(79, 316);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(169, 15);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Закодированное сообщение:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 15);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Введите сообщение, которое надо закодировать:";
+            // 
+            // cipherAnswer
+            // 
+            this.cipherAnswer.Location = new System.Drawing.Point(43, 347);
+            this.cipherAnswer.Name = "cipherAnswer";
+            this.cipherAnswer.Size = new System.Drawing.Size(277, 23);
+            this.cipherAnswer.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 15);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Введите 16-ти символьный ключ:";
+            // 
+            // cipherKey
+            // 
+            this.cipherKey.Location = new System.Drawing.Point(43, 173);
+            this.cipherKey.MaxLength = 16;
+            this.cipherKey.Name = "cipherKey";
+            this.cipherKey.Size = new System.Drawing.Size(277, 23);
+            this.cipherKey.TabIndex = 16;
+            // 
+            // cipherMessage
+            // 
+            this.cipherMessage.Location = new System.Drawing.Point(43, 89);
+            this.cipherMessage.Name = "cipherMessage";
+            this.cipherMessage.Size = new System.Drawing.Size(277, 23);
+            this.cipherMessage.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(277, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Зашифровать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.decipherAnswer);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.decipherMessage);
+            this.groupBox2.Controls.Add(this.decipherKey);
+            this.groupBox2.Location = new System.Drawing.Point(435, 27);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(368, 411);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(126, 312);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(131, 15);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Исходное сообщение:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(106, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(169, 15);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Закодированное сообщение:";
+            // 
+            // decipherAnswer
+            // 
+            this.decipherAnswer.Location = new System.Drawing.Point(63, 347);
+            this.decipherAnswer.Name = "decipherAnswer";
+            this.decipherAnswer.Size = new System.Drawing.Size(267, 23);
+            this.decipherAnswer.TabIndex = 19;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(63, 244);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(267, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Расшифровать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(109, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 15);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Введите 16-ти символьный ключ:";
+            // 
+            // decipherMessage
+            // 
+            this.decipherMessage.Location = new System.Drawing.Point(63, 89);
+            this.decipherMessage.Name = "decipherMessage";
+            this.decipherMessage.Size = new System.Drawing.Size(267, 23);
+            this.decipherMessage.TabIndex = 15;
+            // 
+            // decipherKey
+            // 
+            this.decipherKey.Location = new System.Drawing.Point(63, 173);
+            this.decipherKey.MaxLength = 16;
+            this.decipherKey.Name = "decipherKey";
+            this.decipherKey.Size = new System.Drawing.Size(267, 23);
+            this.decipherKey.TabIndex = 17;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.закодироватьToolStripMenuItem,
+            this.раскодироватьToolStripMenuItem1});
+            this.menuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(925, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // закодироватьToolStripMenuItem
+            // 
+            this.закодироватьToolStripMenuItem.Name = "закодироватьToolStripMenuItem";
+            this.закодироватьToolStripMenuItem.Size = new System.Drawing.Size(95, 20);
+            this.закодироватьToolStripMenuItem.Text = "Закодировать";
+            this.закодироватьToolStripMenuItem.Click += new System.EventHandler(this.закодироватьToolStripMenuItem_Click);
+            // 
+            // раскодироватьToolStripMenuItem1
+            // 
+            this.раскодироватьToolStripMenuItem1.Name = "раскодироватьToolStripMenuItem1";
+            this.раскодироватьToolStripMenuItem1.Size = new System.Drawing.Size(101, 20);
+            this.раскодироватьToolStripMenuItem1.Text = "Раскодировать";
+            this.раскодироватьToolStripMenuItem1.Click += new System.EventHandler(this.раскодироватьToolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox cipherAnswer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox cipherKey;
+        private System.Windows.Forms.TextBox cipherMessage;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox decipherAnswer;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox decipherMessage;
+        private System.Windows.Forms.TextBox decipherKey;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem закодироватьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem раскодироватьToolStripMenuItem1;
+    }
+}
+
---
+++ lab3/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void закодироватьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = true;
+            groupBox2.Visible = false;
+        }
+
+        private void раскодироватьToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = false;
+            groupBox2.Visible = true;
+        }
+
+        // Таблица замены
+        private static byte[,] Table = {
+            {0x4, 0xA, 0x9, 0x2, 0xD, 0x8, 0x0, 0xE, 0x6, 0xB, 0x1, 0xC, 0x7, 0xF, 0x5, 0x3},
+            {0xE, 0xB, 0x4, 0xC, 0x6, 0xD, 0xF, 0xA, 0x2, 0x3, 0x8, 0x1, 0x0, 0x7, 0x5, 0x9},
+            {0x5, 0x8, 0x1, 0xD, 0xA, 0x3, 0x4, 0x2, 0xE, 0xF, 0xC, 0x7, 0x6, 0x0, 0x9, 0xB},
+            {0x7, 0xD, 0xA, 0x1, 0x0, 0x8, 0x9, 0xF, 0xE, 0x4, 0x6, 0xC, 0xB, 0x2, 0x5, 0x3},
+            {0x6, 0xC, 0x7, 0x1, 0x5, 0xF, 0xD, 0x8, 0x4, 0xA, 0x9, 0xE, 0x0, 0x3, 0xB, 0x2},
+            {0x4, 0xB, 0xA, 0x0, 0x7, 0x2, 0x1, 0xD, 0x3, 0x6, 0x8, 0x5, 0x9, 0xC, 0xF, 0xE},
+            {0xD, 0xB, 0x4, 0x1, 0x3, 0xF, 0x5, 0x9, 0x0, 0xA, 0xE, 0x7, 0x6, 0x8, 0x2, 0xC},
+            {0x1, 0xF, 0xD, 0x0, 0x5, 0x7, 0xA, 0x4, 0x9, 0x2, 0x3, 0xE, 0x6, 0xB, 0x8, 0xC}
+        };
+
+
+        // Функция строит из строки длиной 16 символов ключ длиной 8 символов (по 32 бита)
+        static List<UInt32> DivideKeyInto8Pieces(string key16Pieces)
+        {
+            List<UInt32> key8Pieces = new List<UInt32>();
+            for (int keyIndex = 0; keyIndex < key16Pieces.Length; keyIndex += 2)
+            {
+                // Переводим в двоичную систему исчисления и добавляем нулей слева, чтобы
+                // длина была 16 символов (16 бит)
+                string binarLeftPart = Convert.ToString(key16Pieces[keyIndex], 2).PadLeft(16, '0');
+                string binarRightPart = Convert.ToString(key16Pieces[keyIndex + 1], 2).PadLeft(16, '0');
+                // Склеиваем биты левой и правой частей и конвертируем в число
+                key8Pieces.Add(Convert.ToUInt32(binarLeftPart + binarRightPart, 2));
+            }
+            // Если пользователь ввел меньше 16 символов, то докидываем нулей
+            while (key8Pieces.Count != 8)
+                key8Pieces.Add(0);
+            key8Pieces.Reverse();
+            return key8Pieces;
+        }
+
+        // Составляем из строки блок в 64 бита (массив из двух блоков по 32)
+        static List<UInt32> CombineTextTo64BitsBlock(string input)
+        {
+            string binarLeftPart1, binarLeftPart2, binarRightPart1, binarRightPart2;
+            // Каждый символ переводим в биты (16 бит)
+            if (input.Length > 0)
+                binarLeftPart1 = Convert.ToString(input[0], 2).PadLeft(16, '0');
+            else
+                binarLeftPart1 = new string('0', 16);
+            if (input.Length > 1)
+                binarLeftPart2 = Convert.ToString(input[1], 2).PadLeft(16, '0');
+            else
+                binarLeftPart2 = new string('0', 16);
+            if (input.Length > 2)
+                binarRightPart1 = Convert.ToString(input[2], 2).PadLeft(16, '0');
+            else
+                binarRightPart1 = new string('0', 16);
+            if (input.Length > 3)
+                binarRightPart2 = Convert.ToString(input[3], 2).PadLeft(16, '0');
+            else
+                binarRightPart2 = new string('0', 16);
+            // Соединяем биты первых двух букв и последних двух 
+            string binarLeftPart = binarLeftPart1 + binarLeftPart2;
+            string binarRightPart = binarRightPart1 + binarRightPart2;
+
+            // Возвращаем 2 блока по 32 бита
+            return new List<UInt32> {
+                Convert.ToUInt32(binarLeftPart, 2),
+                Convert.ToUInt32(binarRightPart, 2)
+            };
+        }
+
+        // Функция делит двоичный код числа на несколько частей по pieceSize знаков 
+        static List<string> DivideBinaryIntoBlocks(string binaryNumber, int pieceSize)
+        {
+            List<string> blocks = new List<string>();
+            for (int i = 0; i < binaryNumber.Length; i += pieceSize)
+                blocks.Add(binaryNumber.Substring(i, pieceSize));
+            return blocks;
+        }
+
+        // Функция принимает на вход массив из побитовых разложений чисел
+        // Конвертирует каждое разложение в число и возвращает массив из получившихся чисел
+        static List<UInt32> ConvertBlocksToNumbers(List<string> bitBlocks)
+        {
+            List<UInt32> result = new List<UInt32>();
+            foreach (string block in bitBlocks)
+                result.Add(Convert.ToUInt32(block, 2));
+            return result;
+        }
+
+        // Функция заменяет биты числа 32 битного числа number в соответствии с таблицей Table
+        private static void ChangeBitsWithTable(ref UInt32 num32bit)
+        {
+            // Преобразование числа в двоичное представление
+            string binaryNumber = Convert.ToString(num32bit, 2).PadLeft(32, '0');
+            // Разбиение двоичного представления на блоки по 4 бита
+            List<string> bitBlocks = DivideBinaryIntoBlocks(binaryNumber, 4);
+            // Конвертируем блоки в числа
+            List<UInt32> numbersArray = ConvertBlocksToNumbers(bitBlocks);
+            numbersArray.Reverse();
+            List<string> newBitBlocks = new List<string>();
+            // Заменяем биты в соответствии с таблицей 
+            for (int i = 0; i < numbersArray.Count; i++)
+                newBitBlocks.Add(Convert.ToString(Table[i, numbersArray[i]], 2).PadLeft(4, '0'));
+            newBitBlocks.Reverse();
+            // Новые биты переводим обратно в числа
+            num32bit = Convert.ToUInt32(string.Join("", newBitBlocks), 2);
+
+        }
+
+        // Кодирование
+        private string Cipher(string message, string key)
+        {
+            string answer = "";
+            List<UInt32> key8Pieces = DivideKeyInto8Pieces(key);
+            char[] messageArray = message.ToCharArray();
+            while (messageArray.Length > 0)
+            {
+                // Получаем новые 4 символа из сообщения
+                string pieceOfMessage = string.Join("", messageArray.Take(4));
+                messageArray = messageArray.Skip(4).ToArray();
+                int i = 1;
+                // Склеиваем пары символов и получаем 2 32 битовых числа
+                List<UInt32> blocks = CombineTextTo64BitsBlock(pieceOfMessage);
+                // Работаем с левой и правой частью
+                UInt32 l = blocks[0];
+                UInt32 r = blocks[1];
+                UInt32 v;
+                while (i <= 32)
+                {
+                    v = r;
+                    int j = i < 25 ? (i - 1) % 8 : (32 - i) % 8;
+                    r = (r + key8Pieces[j]) % UInt32.MaxValue;
+                    ChangeBitsWithTable(ref r);
+                    r = (r << 11) | (r >> (32 - 11));
+                    r = r ^ l;
+                    l = v;
+                    i++;
+                }
+                // Соединяем биты правой и левой частей
+                string block64Bit = Convert.ToString(r, 2).PadLeft(32, '0') + Convert.ToString(l, 2).PadLeft(32, '0');
+                // Разбиваем большой блок на 4 маленьких по 16 символов
+                List<string> bitBlocksString = DivideBinaryIntoBlocks(block64Bit, 16);
+                foreach (string bitBlock in bitBlocksString)
+                    answer += Convert.ToChar(Convert.ToUInt16(bitBlock, 2)) + "";
+            }
+            return answer;
+        }
+
+
+        // Раскодирование
+        private string Decipher(string message, string key)
+        {
+            string answer = "";
+            List<UInt32> key8Pieces = DivideKeyInto8Pieces(key);
+            char[] messageArray = message.ToCharArray();
+            while (messageArray.Length > 0)
+            {
+                string pieceOfMessage = string.Join("", messageArray.Take(4));
+                messageArray = messageArray.Skip(4).ToArray();
+                int i = 1;
+                // Склеиваем пары символов и получаем 2 32 битовых числа
+                List<UInt32> blocks = CombineTextTo64BitsBlock(pieceOfMessage);
+                // Работаем с левой и правой частью
+                UInt32 l = blocks[0];
+                UInt32 r = blocks[1];
+                UInt32 v;
+                while (i <= 32)
+                {
+                    // Делаем тоже самое, но j берем другой
+                    v = r;
+                    int j = i <= 8 ? (i - 1) % 8 : (32 - i) % 8;
+                    r = (r + key8Pieces[j]) % UInt32.MaxValue;
+                    ChangeBitsWithTable(ref r);
+                    r = (r << 11) | (r >> (32 - 11));
+                    r = r ^ l;
+                    l = v;
+                    i++;
+                }
+                // Соединяем биты правой и левой частей
+                string block64Bit = Convert.ToString(r, 2).PadLeft(32, '0') + Convert.ToString(l, 2).PadLeft(32, '0');
+                // Разбиваем большой блок на 4 маленьких по 16 символов
+                List<string> bitBlocksString = DivideBinaryIntoBlocks(block64Bit, 16);
+                foreach (var bitBlock in bitBlocksString)
+                    answer += Convert.ToChar(Convert.ToUInt16(bitBlock, 2)) + "";
+            }
+            return answer;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string message = cipherMessage.Text;
+            string key = cipherKey.Text;
+            cipherAnswer.Text = Cipher(message, key);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string message = decipherMessage.Text;
+            string key = decipherKey.Text;
+            decipherAnswer.Text = Decipher(message, key);
+        }
+    }
+}
